331da22fa891ac7565a9addeea4923777ba8814f
Hamed Rostami <H4medRostami@gmail.com>
2018-08-14 13:57:59 +0430

    Create wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,25 @@
+class list_wrapper:
+    def __init__(self,list_value=[],slice_num=2):
+        self._list_value=list_value
+        self.slice_num=slice_num
+        
+    def wrapper(self):
+        for i in range(len(self._list_value)):
+            if i == self.slice_num-1 :
+                print(self._list_value[:self.slice_num])
+                del self._list_value[:self.slice_num]
+                return self.wrapper()
+    @property
+    def list_value(self):
+        print("Getting value")
+        return self._list_value
+
+    @list_value.setter
+    def list_value(self, value):
+        if type(value) != list:
+            raise ValueError("only list type")
+        print("Setting value")
+        self._list_value = value
+        
+        
+      
