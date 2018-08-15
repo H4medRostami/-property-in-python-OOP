ea26ac42dca8cff7ec41a8e093f9564ac901cc72
Hamed Rostami <H4medRostami@gmail.com>
2018-08-15 14:11:27 +0430

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,25 +1,25 @@
 class list_wrapper:
     def __init__(self,list_value=[],slice_num=2):
-        self._list_value=list_value # _ before variable means private class variable.
+        self.__list_value=list_value # _ before variable means private class variable.
         self.slice_num=slice_num
         
     def wrapper(self):
-        for i in range(len(self._list_value)):
+        for i in range(len(self.__list_value)):
             if i == self.slice_num-1 :
-                print(self._list_value[:self.slice_num])
-                del self._list_value[:self.slice_num]
+                print(self.__list_value[:self.slice_num])
+                del self.__list_value[:self.slice_num]
                 return self.wrapper()
     @property
     def list_value(self):
         print("Getting value")
-        return self._list_value
+        return self.__list_value
 
     @list_value.setter
     def list_value(self, value):
         if type(value) != list:
             raise ValueError("only list type")
         print("Setting value")
-        self._list_value = value
+        self.__list_value = value
         
         
       
